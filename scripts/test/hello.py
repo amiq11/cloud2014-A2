ed1474c89f5a8d8823d50e9c26917b3c3c1f0aeb
Makoto Shimazu <makoto.shimaz@gmail.com>
2014-05-09 19:14:27 +0900

    Added hello

---
+++ scripts/test/hello.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+#
+# Author:   Makoto Shimazu <makoto.shimaz@gmail.com>
+# URL:      https://amiq11.tumblr.com
+# License:  MIT License
+# Created:  2014-05-09
+#
+print( "Hello.py" )
