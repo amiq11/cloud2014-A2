9bb15276273ba41ee0d5a2044ec29d33848a38d6
Takuya Okamoto <characterokamo@gmail.com>
2014-05-13 01:13:19 -0700

    views.py:add post request handling, create.html:include header and footer

--- vmfront/vmmanager/views.py
+++ vmfront/vmmanager/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from xml.dom.minidom import parseString
 from django.core.context_processors import csrf
+from django import forms
 
 import libvirt
 # Create your views here.
@@ -27,8 +28,68 @@ def status(request, vmname):
                                   'graphics_port': parsed.getElementsByTagName('graphics')[0].getAttribute('port')
                               })
 
-def create(request):
-    data = {}
-    data.update(csrf(request))
-    return render_to_response('vmmanager/create.html', data)
+class VMForm(forms.Form):
+        name = forms.CharField()
+        memory = forms.ChoiceField(choices=[(256, "256"), (512, "512"), (1024, "1024")])
+        vcpu = forms.ChoiceField(choices=[(1, "1"), (2, "2"), (3, "3")])
+        disk = forms.ChoiceField(choices=[(2, "2"), (5, "5"), (10, "10"), (20, "20")])
+        os = forms.ChoiceField(choices=[("", "")])
 
+def create(request):
+        if request.method == "POST":
+                form = VMForm(request.POST)
+                if form.is_valid():
+                        # code of creating vm
+                        # and redirecting to vnc
+                        con = libvirt.open('qemu:///system')
+			print("aaaaa")
+                        domain = con.defineXML(D_XML % form.cleaned_data)
+                        status = domain.create()
+                        if status != -1:
+                                # TODO
+                                # return redirect to vnc
+                                pass
+        else:
+                form = VMForm()
+        c = {"form": form}
+        return render_to_response('vmmanager/create.html', c,
+                                                          context_instance=RequestContext(request))
+
+
+
+D_XML = """\
+<domain type="kvm"> <!-- Domain Type -->
+        <name>(%name)s</name> <!-- name for vm -->
+        <uuid></uuid> <!-- global identifier for virtual machines. if define/create a new machine, a random UUID is generated-->
+
+        <os> <!-- Bootloader -->
+                <type>hvm</type>  <!-- Full Virtualization -->
+                <boot dev='cdrom'/> <!-- Boot Device -->
+                <boot dev='hd' />
+        </os>
+
+        <vcpu>(%vcpu)d</vcpu> <!-- CPU allocation -->
+        <memory unit='MiB'>(%memory)d</memory> <!-- Maximum Memory Allocation Size -->
+        <currentMemory unit='MiB'>(%memory)d</currentMemory> <!-- Current Memory Allocation -->
+
+        <devices> <!-- devices provided to the guest domain -->
+                <emulator>/usr/bin/kvm</emulator> <!--  -->
+                <disk type='file' device='disk'> <!-- type:underlying source for the disk, device:how the disk is exposed to the guest OS -->
+                        <source file='/var/lib/libvirt/images/sparse.img' />
+                        <target dev='hda' />
+                </disk>
+                <disk type='file' device='cdrom'>
+                        <source file='/var/local/kvm/installer/debian-7.5.0-amd64-netinst.iso' />
+                        <target dev='hda' />
+                        <readonly/>
+                        <address type='drive' controller='0' bus='1' target='0' unit='0'/>
+                </disk>
+                <interface type='network'>
+                        <source network='default' />
+                        <mac address='24:42:53:21:52:45' />
+                </interface>
+                <graphics type='vnc' port='-1' keymap='ja' />
+        </devices>
+
+</domain>
+"""\
