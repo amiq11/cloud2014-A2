79494b1d765b3fe4b40183ab6036c6e144c03c53
user <user@group4-2.(none)>
2014-05-14 03:20:38 +0900

    Added server configuration

--- vmfront/vmfront/wsgi.py
+++ vmfront/vmfront/wsgi.py
@@ -8,7 +8,15 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+
+"sys.path.append('/home/user/cloud2014-A2/vmfront/vmfront')"
+"sys.path.append('/home/user/cloud2014-A2/vmfront/vmmanager')"
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vmfront.settings")
+
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+
+
