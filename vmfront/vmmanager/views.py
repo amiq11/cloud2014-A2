8ed82fda3c06e6eee093c70b953a2234bbea9828
yoshiro <yoshiro@yoshiro-VirtualBox.(none)>
2014-05-13 21:14:27 +0900

    ちょっと足した

--- vmfront/vmmanager/views.py
+++ vmfront/vmmanager/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
+from django.http import HttpResponseRedirect
 from xml.dom.minidom import parseString
 import libvirt
 # Create your views here.
@@ -18,8 +19,15 @@ def status(request, vmname):
     con = libvirt.open('qemu:///system')
     dom = con.lookupByName(vmname)
     parsed = parseString(dom.XMLDesc(libvirt.VIR_DOMAIN_XML_SECURE))
-    return render_to_response('vmmanager/status.html',
-                              {
+
+    #if request.method == 'POST':
+    if request.POST["shutdown"] == "true":
+        dom.destroy()
+        return HttpResponseRedirect("http://127.0.0.1:8000/vmmanager/")
+     
+    else:
+        return render_to_response('vmmanager/status.html',
+                                  context_instance=RequestContext(request, {
                                   'dom': dom,
                                   'info': dom.info,
                                   'mem': dom.memoryStats,
@@ -27,7 +35,7 @@ def status(request, vmname):
 				  'max_cpu': dom.maxVcpus,
                                   'getinfo': con.getInfo,
                                   'graphics_port': parsed.getElementsByTagName('graphics')[0].getAttribute('port')
-                              })
+                              }))
 
 def create(request):
     return render_to_response('vmmanager/create.html')
