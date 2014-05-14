b9d9efc9acd09270356d444ac69ae871ebb3a0b1
yoshiro <yoshiro@yoshiro-VirtualBox.(none)>
2014-05-14 17:39:10 +0900

    いくつか無駄な変更点を修正しました

--- vmfront/vmmanager/views.py
+++ vmfront/vmmanager/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render_to_response, get_object_or_404
+from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
@@ -21,14 +21,13 @@ def status(request, vmname):
     dom = con.lookupByName(vmname)
     parsed = parseString(dom.XMLDesc(libvirt.VIR_DOMAIN_XML_SECURE))
 
-    #if request.method == 'POST':
     try:
         if request.POST["shutdown"] == "true":
             dom.destroy()
             return HttpResponseRedirect(reverse('vmmanager.views.index'))
         
     except KeyError:
-        #context_instance=RequestContext(request, 
+        
         return render_to_response('vmmanager/status.html',
                                   context_instance=RequestContext(request, {
                                   'dom': dom,
