f2af7caf2762f54e1432a78486552b6e6d9e11f1
yoshiro <yoshiro@yoshiro-VirtualBox.(none)>
2014-05-14 18:17:00 +0900

    停止中のVMも表示されるようにしました

--- vmfront/vmmanager/views.py
+++ vmfront/vmmanager/views.py
@@ -7,17 +7,19 @@ import libvirt
 # Create your views here.
 
 def index(request):
-    con = libvirt.open('qemu+tls://g4hv.exp.ci.i.u-tokyo.ac.jp/system')
+    #con = libvirt.open('qemu+tls://g4hv.exp.ci.i.u-tokyo.ac.jp/system')
+    con = libvirt.open('qemu:///system')
     vmdoms = []
-    for id in con.listDomainsID():
-        dom = con.lookupByID(id)
+    for name in con.listDefinedDomains():
+        dom = con.lookupByName(name)
         vmdoms.append(dom)
     return render_to_response('vmmanager/index.html',
                               {'vmdoms': vmdoms},
                               context_instance=RequestContext(request))
 
 def status(request, vmname):
-    con = libvirt.open('qemu+tls://g4hv.exp.ci.i.u-tokyo.ac.jp/system')
+    #con = libvirt.open('qemu+tls://g4hv.exp.ci.i.u-tokyo.ac.jp/system')
+    con = libvirt.open('qemu:///system')
     dom = con.lookupByName(vmname)
     parsed = parseString(dom.XMLDesc(libvirt.VIR_DOMAIN_XML_SECURE))
 
